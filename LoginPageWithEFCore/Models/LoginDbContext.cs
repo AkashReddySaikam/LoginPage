924feae398def3670c5eee9111a015f91c114b12
AkashReddySaikam <akashreddysaikam@gmail.com>
2024-10-02 15:03:42 +0530

    Add project files.

---
+++ LoginPageWithEFCore/Models/LoginDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LoginPageWithEFCore.Models
+{
+    public partial class LoginDbContext :DbContext
+    {
+        public LoginDbContext()
+        {
+        }
+
+        public LoginDbContext(DbContextOptions<LoginDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Email)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EmpName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ LoginPageWithEFCore/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace LoginPageWithEFCore.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Name")]
+        public string EmpName { get; set; } = null!;
+        [Required]
+        public string Gender { get; set; } = null!;
+        [Required]
+        public int? Age { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
